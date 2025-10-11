32e7af424511c0a065a0340241206f705e2dec7d
W\w <614140134@qq.com>
2025-10-11 17:52:48 +0800

    修改目录结构

---
+++ README.md
@@ -0,0 +1,20 @@
+# Electron 开发的 AI 聊天软件
+
+
+
+## 如何启动项目
+1. 克隆项目到本地
+```bash
+git clone https://github.com/noRainToday/ai-robot-vue.git
+```
+
+2. 安装依赖
+```bash
+cd ai-robot-vue
+npm install
+```
+
+3. 启动项目
+```bash
+npm run dev
+```
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,17 +1,42 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'node:path';
+import { defineConfig, CSSOptions } from "vite";
+import { resolve } from "node:path";
+import autoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config
 export default defineConfig(async () => {
-  const vue = (await import('@vitejs/plugin-vue')).default;
-  const tailwindcss = (await import('@tailwindcss/vite')).default;
+  const vue = (await import("@vitejs/plugin-vue")).default;
+  const tailwindcss = (await import("@tailwindcss/vite")).default;
 
   return {
-    plugins: [vue(), tailwindcss()],
+    plugins: [
+      vue(),
+      tailwindcss(),
+      autoImport({
+        imports: ["vue", "vue-router", "pinia", "@vueuse/core", "vue-i18n"],
+        dts: "renderer/auto-imports.d.ts",
+      }),
+    ],
+    css: {
+      transformer: "lightningcss" as CSSOptions["transformer"],
+    },
+    build: {
+      target: "es2022",
+      publicDir: "public",
+      rollupOptions: {
+        input: [
+          resolve(__dirname, "html/index.html"),
+          resolve(__dirname, "html/setting.html"),
+          resolve(__dirname, "html/dialog.html"),
+        ],
+      },
+    },
     resolve: {
       alias: {
-        '@': resolve(__dirname, 'src')
-      }
-    }
-  }
+        "@common": resolve(__dirname, "common"),
+        "@renderer": resolve(__dirname, "renderer"),
+        "@main": resolve(__dirname, "main"),
+        "@locales": resolve(__dirname, "locales"),
+      },
+    },
+  };
 });
